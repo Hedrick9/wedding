b308eb831e43a17df31c85a4bc72771e8860632d
Russell Hedrick <hedrickrussell@gmail.com>
2025-07-28 22:52:02 -0500

    Build out structure for web page.

--- app.py
+++ app.py
@@ -3,18 +3,30 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html")
+def gate():
+    return render_template("gate.html")
 
-@app.route("/main")
-def main():
-    return render_template("main.html")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
+
+@app.route("/travel_guide")
+def travel_guide():
+    return render_template("travel_guide.html")
 
 @app.route("/rsvp", methods=["POST"])
 def rsvp():
     name = request.form.get("name")
     attending = request.form.get("attending")
     return render_template("thank_you.html", name=name, attending=attending)
+
+@app.route("/faq")
+def faq():
+    return render_template("faq.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
